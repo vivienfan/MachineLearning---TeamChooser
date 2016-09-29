f103e7daf09ce7b7dc565f87f35a34b4c63ef708
Yinghui (Vivien) Fan <yinghui.fan@mail.utoronto.ca>
2016-09-29 02:47:30 -0400

    M2 - complete
    -- Exhaustive Method failed due to lack of computational power and
    memory --
    
    Reverse predictor and averaging method was applied instead.
    PlayerRating.csv was generated with 1000 games instance, 100% matching

--- Application.py
+++ Application.py
@@ -1,46 +1,72 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Sep 27 23:24:58 2016
+Created on Mon Sep 12 13:32:58 2016
 
-@author: shang
+@author: Vivien Y. Fan
 """
 
-def RatingCalculator(csv_data, csv_rating, csv_combination):
+
+def RatingCalculator(csv_data, csv_rating):
     """ This function runs the model with a new set of test data, then update
         the csv file 
     """
     import pandas
-    
-    # game instances
+    import numpy
+                
+    # To-do: run the model with newGame instance
     data = pandas.read_csv(filepath_or_buffer=csv_data, index_col=0)
-    matrixData = pandas.DataFrame.as_matrix(data)  
-    (num_games, dontcare) = matrixData.shape
-    print(matrixData.shape)    
+    matrixData = pandas.DataFrame.as_matrix(data)
+    dic = pandas.Series.from_csv(csv_rating).to_dict()  
+    dic_temp = {}
+    dic_copy = {}  
     
-    # lookup table
-    lookup_table = {}
     (row, col) = matrixData.shape
-    num_players = 0
+    
+    # first, assign all the new players with an initial rating 5
     for x in range (0, row):
         for y in range(0, col-2):
-            if matrixData[x,y] not in lookup_table:
-                lookup_table[matrixData[x,y]] = num_players
-                num_players += 1           
-    print(lookup_table)  
+            if matrixData[x,y] not in dic:
+                dic[matrixData[x,y]] = 7        # if set to 5, all numbers are off by 2, a bias in the given ratings
+                dic_temp[matrixData[x,y]] = []
 
-    # rating combination per game
-    combination = pandas.read_csv(csv_combination, header=None)
-    matrixCombination = pandas.DataFrame.as_matrix(combination)
-    print(matrixCombination)
+    i = 0
+    # loop until ratings stop updating
+    keep_running = True
+    while keep_running:
+        dic_copy = dic.copy()
+        # loop through all instances tofind a posible rating for every player in each game
+        for x in range (0, row):
+            sumA = 0
+            sumB = 0     
+            for k in range (0, 6):
+                sumA += dic[matrixData[x, k]]
+                sumB += dic[matrixData[x, k+6]]
+                
+            avgA = sumA / 6
+            avgB = sumB / 6   
+            scoreA = matrixData[x, -2]
+            scoreB = matrixData[x, -1]      
+            x_adjustment = (((scoreA - scoreB) / 2 - (avgA - avgB)) / 2) * 11   
+            for k in range (0, 6):
+                # team a: avgA' = avgA + x
+                dic_temp[matrixData[x, k]].append(dic[matrixData[x, k]] + x_adjustment)
+                # team b: avgB' = avgB - x
+                dic_temp[matrixData[x, k+6]].append(dic[matrixData[x, k+6]] - x_adjustment)
+
+        # update the rating with the average
+        for key in dic:
+            dic[key] = int(numpy.mean(dic_temp[key]))
     
-    # loop through all game instances
-    for g in range (0, num_games):
-        # For each, loop through all combination, 
-        # and find all possible combination
-        # use lookup table to append to possible rating combinations
-        score_diff = matrixData[g][-2] - matrixData[g][-1]  # team A - team B
+        # check if the ratings stop updating
+        still_updating = False
+        for key in dic:
+            if dic[key] != dic_copy[key]:
+                still_updating = True;
+        if still_updating == False:
+            keep_running = False;
         
-    
-    
-                    
+    print(i)        
+    player_ratings = pandas.Series(dic)
+    player_ratings.to_csv(csv_rating)
     return
+    
--- Main.py
+++ Main.py
@@ -13,15 +13,15 @@ def main():
     csvFile_rawData = r'C:\Users\shang\Documents\GitHub\Synthetic-Data-Generator-TeamChooser\game_stances.csv'
     csvFile_processedData = r'C:\Users\shang\Documents\GitHub\MachineLearning---TeamChooser\ProcessedData.csv'
     csvFile_playerRating =  r'C:\Users\shang\Documents\GitHub\MachineLearning---TeamChooser\PlayerRating.csv'
-    csvFile_combination = r'C:\Users\shang\Documents\GitHub\MachineLearning---TeamChooser\Combinations.csv'
+#    csvFile_combination = r'C:\Users\shang\Documents\GitHub\MachineLearning---TeamChooser\Combinations.csv'
     # step 1: preprocessing and feature extracting
-    Preprocessor.RawDataProcessor(csvFile_rawData, csvFile_processedData, 10) 
+    Preprocessor.RawDataProcessor(csvFile_rawData, csvFile_processedData, 1000) 
     
     # step 2: Training (offline) - machine learning system (iterative process)
 #    MachineLearningAlgorithm.IterativeTrainingSystem()
   
     # step 3: Testing (online)
-    Application.RatingCalculator(csvFile_processedData, csvFile_playerRating, csvFile_combination)
+    Application.RatingCalculator(csvFile_processedData, csvFile_playerRating)
     return
     
   
